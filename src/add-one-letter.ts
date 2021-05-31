e9dc4af829e7b8a37a7cad8b80282d123e175c89
Greg Finley <greg.finley@lambdaschool.com>
2021-05-30 20:49:19 -0700

    Read in all words

--- src/add-one-letter.ts
+++ src/add-one-letter.ts
@@ -1,1 +1,9 @@
-console.log('Hello world')
+import axios from "axios";
+
+async function main() {
+    const allWords = await axios.get('https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json');
+
+    console.log(Object.keys(allWords.data).length);
+}
+
+main();
