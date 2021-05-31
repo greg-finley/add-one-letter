7de328f3ea9356f87fa431048314550096ac8a16
Greg Finley <greg.finley@lambdaschool.com>
2021-05-30 21:54:50 -0700

    possibleNextWords

--- src/add-one-letter.ts
+++ src/add-one-letter.ts
@@ -1,5 +1,13 @@
 import axios from "axios";
 
+export const letters = [
+    'a', 'b', 'c', 'd', 'e', 'f',
+    'g', 'h', 'i', 'j', 'k', 'l',
+    'm', 'n', 'o', 'p', 'q', 'r',
+    's', 't', 'u', 'v', 'w', 'x',
+    'y', 'z'
+  ];
+
 async function main() {
     const allWords = (await axios.get<Record<string, number>>('https://raw.githubusercontent.com/dwyl/english-words/master/words_dictionary.json')).data;
 
@@ -7,4 +15,24 @@ async function main() {
     console.log(allWords.sgnaingiasning);
 }
 
-main();
+export function possibleNextWords(word: string, letters: string[]) {
+    const possibleWords: Set<string> = new Set();
+
+    // For every letter in the alphabet, add it to the front of my word
+    letters.forEach((l) => possibleWords.add(`${l}${word}`));
+
+    // For every letter in my word, insert a letter after it, trying all letters in the alphabet
+    word.split('').forEach((wordLetter, index, splitWord) => {
+        const front = splitWord.slice(0, index + 1);
+        const back = splitWord.slice(index + 1);
+        letters.forEach((l) => {
+           possibleWords.add(`${front.join('')}${l}${back.join('')}`)
+        });
+    })
+
+    return Array.from(possibleWords);
+}
+
+if (require.main === module) {
+    main();
+  }
---
+++ test/add-one-letter.spec.ts
@@ -0,0 +1,26 @@
+import { expect } from 'chai';
+import { letters, possibleNextWords } from '../src/add-one-letter'
+
+describe('add-one-letter', () => {
+    describe('possibleNextWords', () => {
+        it('finds possible next words', () => {
+            expect(possibleNextWords('bar', letters)).to.eql([
+                'abar', 'bbar', 'cbar', 'dbar', 'ebar', 'fbar', 'gbar',
+                'hbar', 'ibar', 'jbar', 'kbar', 'lbar', 'mbar', 'nbar',
+                'obar', 'pbar', 'qbar', 'rbar', 'sbar', 'tbar', 'ubar',
+                'vbar', 'wbar', 'xbar', 'ybar', 'zbar', 'baar', 'bcar',
+                'bdar', 'bear', 'bfar', 'bgar', 'bhar', 'biar', 'bjar',
+                'bkar', 'blar', 'bmar', 'bnar', 'boar', 'bpar', 'bqar',
+                'brar', 'bsar', 'btar', 'buar', 'bvar', 'bwar', 'bxar',
+                'byar', 'bzar', 'babr', 'bacr', 'badr', 'baer', 'bafr',
+                'bagr', 'bahr', 'bair', 'bajr', 'bakr', 'balr', 'bamr',
+                'banr', 'baor', 'bapr', 'baqr', 'barr', 'basr', 'batr',
+                'baur', 'bavr', 'bawr', 'baxr', 'bayr', 'bazr', 'bara',
+                'barb', 'barc', 'bard', 'bare', 'barf', 'barg', 'barh',
+                'bari', 'barj', 'bark', 'barl', 'barm', 'barn', 'baro',
+                'barp', 'barq', 'bars', 'bart', 'baru', 'barv', 'barw',
+                'barx', 'bary', 'barz'
+            ]);
+        })
+    })
+})
